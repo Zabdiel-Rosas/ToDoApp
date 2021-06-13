34918c6190c8361deef6dd4710d224a2516ab6ed
Zabdiel-Rosas <zabdiel_rosas@outlook.com>
2021-06-13 17:56:53 -0500

    changes component to display a button that opens a modal component

--- src/components/AddTask/index.js
+++ src/components/AddTask/index.js
@@ -1,37 +1,47 @@
 import React, { Component } from 'react'
 import './style.css'
+import Modal from '../Modal/index'
 
 class AddTask extends Component {
   constructor (props) {
     super(props)
-    this.onChange = props.onChange
-    this.onClick = props.onClick
-    this.state = { task: '' }
-    this.handleChange = this.handleChange.bind(this)
-    this.handleClick = this.handleClick.bind(this)
+    this.state = { showModal: false }
+    this.handleShow = this.handleShow.bind(this)
+    this.handleHide = this.handleHide.bind(this)
   }
 
-  handleChange (e) {
-    this.setState({ task: e.target.value })
+  handleShow () {
+    this.setState({ showModal: true })
   }
 
-  handleClick () {
-    this.onClick(this.state.task)
-    this.setState({ task: '' })
+  handleHide () {
+    this.setState({ showModal: false })
   }
 
   render () {
     return (
       <div className='addingTask'>
-        <input
-          type='text'
-          className='textArea'
-          placeholder='Write new task'
-          value={this.state.task}
-          onChange={this.handleChange}
-        />
-        <div className='addBtnArea'>
-          <button className='btn' onClick={this.handleClick}>Add</button>
+        <div className='addTaskCard'>
+          <button type='button' className='addBtn' onClick={this.handleShow}>+ Add a Task</button>
+          {
+            this.state.showModal &&
+              <Modal>
+                <div className='modal-background'>
+                  <div className='add-task-form'>
+                    <input
+                      type='text'
+                      className='textArea'
+                      placeholder='Write new task'
+                      value={this.state.task}
+                      onChange={this.handleChange}
+                    />
+                    <div className='addBtnArea'>
+                      <button className='btn' onClick={this.handleClick}>Add</button>
+                    </div>
+                  </div>
+                </div>
+              </Modal>
+          }
         </div>
       </div>
     )
