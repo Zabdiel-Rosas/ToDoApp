efa2b75af50ca9df4114465d21e1f826c03af553
Zabdiel Rosas <zabdiel_rosas@outlook.com>
2021-06-14 15:33:31 -0500

    adds logic to handle modal component actions

--- src/components/AddTask/index.js
+++ src/components/AddTask/index.js
@@ -5,9 +5,13 @@ import Modal from '../Modal/index'
 class AddTask extends Component {
   constructor (props) {
     super(props)
-    this.state = { showModal: false }
+    this.state = { showModal: false, task: '' }
+    this.onChange = props.onChange
+    this.onClick = props.onClick
     this.handleShow = this.handleShow.bind(this)
     this.handleHide = this.handleHide.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+    this.handleClick = this.handleClick.bind(this)
   }
 
   handleShow () {
@@ -18,16 +22,27 @@ class AddTask extends Component {
     this.setState({ showModal: false })
   }
 
+  handleChange (e) {
+    this.setState({ task: e.target.value })
+  }
+
+  handleClick () {
+    this.onClick(this.state.task)
+    this.setState({ task: '' })
+    this.handleHide()
+  }
+
   render () {
     return (
-      <div className='addingTask'>
+      <div className='addingTask-container'>
         <div className='addTaskCard'>
-          <button type='button' className='addBtn' onClick={this.handleShow}>+ Add a Task</button>
+          <button type='button' className='addTaskBtn' onClick={this.handleShow}>+ Add a Task</button>
           {
             this.state.showModal &&
               <Modal>
                 <div className='modal-background'>
                   <div className='add-task-form'>
+                    <button className="close-btn" onClick={this.handleHide}>X</button>
                     <input
                       type='text'
                       className='textArea'
