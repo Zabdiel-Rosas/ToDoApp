42780f6e8718252adf7d98085bc2eb50b6c0f8e8
Zabdiel Rosas <zabdiel_rosas@outlook.com>
2021-06-14 15:31:07 -0500

    changes modal to be a generic component, removing logic from it

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -4,23 +4,6 @@ import ReactDom from 'react-dom'
 const modalRoot = document.getElementById('modal-root')
 
 class Modal extends Component {
-  constructor (props) {
-    super(props)
-    this.state = { task: '' }
-    this.onChange = props.onChange
-    this.onClick = props.onClick
-    this.handleChange = this.handleChange.bind(this)
-    this.handleClick = this.handleClick.bind(this)
-  }
-
-  handleChange (e) {
-    this.setState({ task: e.target.value })
-  }
-
-  handleClick () {
-    this.onClick(this.state.task)
-    this.setState({ task: '' })
-  }
 
   render () {
     return ReactDom.createPortal(
